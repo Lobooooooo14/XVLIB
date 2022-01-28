03619349ded033753eb29543034eb45d5e8d4659
Lobooooooo <88998991+Lobooooooo14@users.noreply.github.com>
2022-01-27 21:03:56 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+
+setup(
+  name = 'pornlib',
+  version = '1.0.0',
+  author = 'Lobooooooo',
+  author_email = 'lobooooooo.py@gmail.com',
+  description = 'A Python package for scrapping at xvideos.com easily.',
+  long_description = '',
+  url = 'https://github.com/lobooooooo14/XVLIB', 
+  project_urls = {
+    'Github':'https://github.com/lobooooooo14/XVLIB'
+  },
+  keywords = 'scraper scraping-websites xvideos english-language english brazilian-portuguese português portuguese xvideos-downloader xvideos',
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Development Status :: 5 - Production/Stable',
+    'Natural Language :: Portuguese (Brazilian)',
+    'Operating System :: Android',
+    'Programming Language :: Python :: 3.10'
+    
+    ],
+  packages = find_packages()
+)
