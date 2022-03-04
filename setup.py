90b338832f946af100b925c513a1fbf20708ef0c
Lobooooooo <88998991+Lobooooooo14@users.noreply.github.com>
2022-03-04 12:14:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,23 +3,19 @@ from setuptools import setup, find_packages
 
 
 setup(
-  name = 'pornlib',
+  name = 'XVLIB',
   version = '1.0.0',
   author = 'Lobooooooo',
   license = 'GPL-3.0 License',
   author_email = 'lobooooooo.py@gmail.com',
-  description = 'A Python package for scrapping at xvideos and others sites easily.',
-  long_description = 'see the documentation: https://lobooooooo14.github.io/pornlib/',
+  description = 'A Python package for scrapping at xvideos easily.',
+  long_description = 'see the documentation: https://lobooooooo14.github.io/XVLIB/',
   url = 'https://github.com/lobooooooo14/XVLIB', 
   project_urls = {
     'Github':'https://github.com/lobooooooo14/XVLIB'
   },
   keywords = 'scraper scraping-websites xvideos english-language english brazilian-portuguese português portuguese xvideos-downloader xvideos',
   classifiers = [
-    'Development Status :: 4 - Beta',
-    'Development Status :: 5 - Production/Stable',
-    'Natural Language :: Portuguese (Brazilian)',
-    'Operating System :: Android',
     'Programming Language :: Python :: 3.10'
     
     ],
